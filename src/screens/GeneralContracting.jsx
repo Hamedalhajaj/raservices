404d5b429b04718ee63b7b97fa06adc1475a6c0b
Hossam Mohamed <46819835+ThePinkHatDev@users.noreply.github.com>
2021-09-03 13:09:14 -0400

    Removed an extra space.

--- src/screens/GeneralContracting.jsx
+++ src/screens/GeneralContracting.jsx
@@ -11,19 +11,19 @@ const GeneralContracting = props => {
   const payload = [
     {
       svg: interior,
-      text: 'Here at R. A Services LLC, we strive to give you that "Dream Home" feeling with our extensive knowledge and experience with interior renovations. Whether you want to update your kitchen or bathroom, change out your carpet for beautiful hardwood floors, or just update your light fixtures, our team will help make your house feel like a home.',
+      text: 'Here at R.A Services LLC, we strive to give you that "Dream Home" feeling with our extensive knowledge and experience with interior renovations. Whether you want to update your kitchen or bathroom, change out your carpet for beautiful hardwood floors, or just update your light fixtures, our team will help make your house feel like a home.',
       alt: 'interior',
       headerText: 'Interior Renovation'
     },
     {
       svg: exterior,
-      text: "If the outside of your house looks weathered and faded, our team at R. A Services LLC can help revamp your exterior and bring your home more curb appeal. Let us work with you to update your home's siding, roof, or even bring new life to your cracked driveway or sidewalk.",
+      text: "If the outside of your house looks weathered and faded, our team at R.A Services LLC can help revamp your exterior and bring your home more curb appeal. Let us work with you to update your home's siding, roof, or even bring new life to your cracked driveway or sidewalk.",
       alt: 'exterior',
       headerText: 'Exterior Renovation'
     },
     {
       svg: home,
-      text: 'Is your house outdated? Do you love the house but wish it felt more like a home? Our #1 goal at R. A Services LLC is to create for you  a home that you love. A few updates can really transform a room and bring it back to life!',
+      text: 'Is your house outdated? Do you love the house but wish it felt more like a home? Our #1 goal at R.A Services LLC is to create for you  a home that you love. A few updates can really transform a room and bring it back to life!',
       alt: 'home',
       headerText: 'Home Revamp'
     }
--- src/screens/RemodelingScreen.jsx
+++ src/screens/RemodelingScreen.jsx
@@ -11,13 +11,13 @@ const RemodelingScreen = props => {
   const payload = [
     {
       svg: kitchen,
-      text: 'Let us bring your dream kitchen to life. No matter if you need a few small updates or an entire kitchen makeover, our team at R. A Services LLC can work with you to create a custom design you are sure to love.',
+      text: 'Let us bring your dream kitchen to life. No matter if you need a few small updates or an entire kitchen makeover, our team at R.A Services LLC can work with you to create a custom design you are sure to love.',
       alt: 'basement',
       headerText: 'Kitchen'
     },
     {
       svg: bathroom,
-      text: 'Looking to update your bathroom? Adding new tile to the floor or walls is a sure fire way to brighten up any bathroom. Swap out your standard sink for a beautiful vanity to add aesthetics and storage. Whatever changes you are looking to make, let R. A Services LLC help you make that vision a reality.',
+      text: 'Looking to update your bathroom? Adding new tile to the floor or walls is a sure fire way to brighten up any bathroom. Swap out your standard sink for a beautiful vanity to add aesthetics and storage. Whatever changes you are looking to make, let R.A Services LLC help you make that vision a reality.',
       alt: 'bathroom',
       headerText: 'Bathroom'
     },
